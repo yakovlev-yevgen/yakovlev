b725dd9ca5dce84c596cdccfe57b1a8bf9c642ce
Nadiia Khoptiuk <holineinadia@gmail.com>
2024-06-15 10:36:57 +0300

    fix validation errors

--- components/ui-kit/Contact/Contact.tsx
+++ components/ui-kit/Contact/Contact.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import cn from 'classnames';
+
 import { ICONS } from '../Icons';
+
 import common from 'data/common.json';
 
 type ContactProps = {
@@ -18,7 +20,7 @@ export const Contact: React.FC<ContactProps> = ({
         'inline-block font-ui_roboto text-ui_reg_20 text-ui_light notXl:w-10 notXl:h-10 notXl:flex notXl:items-center notXl:justify-center notXl:bg-ui_accent notXl:rounded-[50%] notXl:border notXl:border-transparent notXl:hocus:bg-ui_light notXl:hocus:border-ui_accent notXl:text-white notXl:hocus:text-ui_accent notXl:transition-colors notXl:duration-250 xl:cursor-default xl:pointer-events-none',
         className,
       )}
-      href={`tel:${phoneNumber}`}
+      href={`tel:${phoneNumber.replace(' ', '')}`}
       aria-label={common.contacts.phoneAria}
     >
       <ICONS.PHONE className="hidden w-[25px] h-[25px] stroke-current notXl:block" />
